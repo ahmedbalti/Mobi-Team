1b07081bfd80aea24e6c1f346da04986bc817147
nermine <nermin.fattouch@esprit.tn>
2023-11-14 05:51:05 +0100

    update

--- app/src/main/java/com/esprit/rentacar/HomeFeedBackActivity.java
+++ app/src/main/java/com/esprit/rentacar/HomeFeedBackActivity.java
@@ -1,7 +1,7 @@
 package com.esprit.rentacar;
 
 import androidx.appcompat.app.AppCompatActivity;
-<<<<<<< HEAD
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
@@ -16,27 +16,24 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
-=======
-
 import android.content.Intent;
->>>>>>> 27ec3c4948ea7d7159c37a03a0003395b4416bac
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class HomeFeedBackActivity extends AppCompatActivity {
 
-<<<<<<< HEAD
-    Button send, feedback,other;
-=======
-    Button sendBtn ;
->>>>>>> 27ec3c4948ea7d7159c37a03a0003395b4416bac
+
+    Button send, feedback, other;
+
+    Button sendBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_feed_back2);
-<<<<<<< HEAD
+
         feedback = (Button) findViewById(R.id.feedback);
         send = findViewById(R.id.send);
         other = findViewById(R.id.other);
@@ -73,26 +70,9 @@ public class HomeFeedBackActivity extends AppCompatActivity {
 
     //getSupportActionBar().setTitle("send");
 
-        public void openFeedBack(View view){
-            startActivity(new Intent(HomeFeedBackActivity.this,FeedBackActivity.class));
-=======
+    public void openFeedBack(View view) {
+        startActivity(new Intent(HomeFeedBackActivity.this, FeedBackActivity.class));
 
-
-         //getSupportActionBar().setTitle("send");
-
-            sendBtn = findViewById(R.id.send) ;
-
-            sendBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent creditCardIntent = new Intent(HomeFeedBackActivity.this, FeedBackActivity.class);
-                    startActivity(creditCardIntent);
-                }
-            });
-        }
-        public void openFeedBack(View view){
-            startActivity(new Intent(this,FeedBackActivity.class));
->>>>>>> 27ec3c4948ea7d7159c37a03a0003395b4416bac
-        }
 
     }
+}
